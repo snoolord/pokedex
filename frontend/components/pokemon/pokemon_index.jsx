f9c6ad4c6b0432f782970b539e9f017db0379242
App Academy Student <student@appacademy.io>
2016-10-26 17:20:11 -0700

    Actually finish phase 4

--- frontend/components/pokemon/pokemon_index.jsx
+++ frontend/components/pokemon/pokemon_index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import PokemonIndexItem from './pokemon_index_item';
 
 const PokemonIndex = ({pokemon}) => {
   return (
     <ul>
-      {pokemon.map( (pk, idx) => (<li key={idx}>{pk.name}</li>))}
+      {pokemon.map( (pk, idx) => (<PokemonIndexItem key={pk.id} pokemon={pk}/>))}
     </ul>
   );
 };
